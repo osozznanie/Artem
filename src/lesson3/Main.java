c5eb8ae63a0aa60e3b6119743db19625735086f2
Влад Черторыгин <vladcertorygin@MacBook-Air.local>
2023-07-11 19:11:20 +0300

    Initial commit

---
+++ src/lesson3/Main.java
@@ -0,0 +1,22 @@
+package lesson3;
+
+public class Main {
+    public static void main(String[] args) {
+        // i; або
+        // i == &&
+        // або == ||
+
+        int a = 22;
+        int b = 285;
+        if (a % 2 == 0 && b % 2 == 0){
+            System.out.println("Привіт");
+        }
+
+        if (a % 2 == 0 || b % 2 == 0){
+            System.out.println("Hello");
+        }
+
+
+
+    }
+}
